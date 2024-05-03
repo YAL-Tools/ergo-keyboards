f9948d2fd2f2cf9f18a57a4b8109439899f19165
DavidMirror <moorelinux@gmail.com>
2024-05-03 12:30:20 -0700

    Completed keyboard list

--- docs/mcus.csv.js
+++ docs/mcus.csv.js
@@ -1,13 +1,13 @@
 // https://docs.google.com/spreadsheets/d/1n3GiSTfl841fM2TmFexftcDNppRMkwo9DEgcTyAcpHA/edit
 window.mcuData = `Name & photo,#of Microcontrollers,Footprint,Total Pins,Microcontroller
-0xPM,2,Onboard/SMD, ,!CH552
-3w6,2,Onboard/SMD, ,ATmega32U4
+0xPM,2,Onboard/SMD,0,!CH552
+3w6,2,Onboard/SMD,0,ATmega32U4
 7x5-split-ergo,2,Pro Micro,24,ATmega32U4
 Abomination,2,Elite-C,29,"ATmega32U4, RP2040"
 Abyss,2,Nice!Nano,29,nRF52840
 Aether,1,!Teensy++ 2.0,54,!Teensy++ 2.0
 Ajisai,1,Nice!Nano,29,nRF52840
-Alien Invader,1,Onboard/SMD, ,RP2040
+Alien Invader,1,Onboard/SMD,0,RP2040
 Allium58,2,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
 Anglerfish,2,Nice!Nano,29,nRF52840
 Apodiformbird,1,RP2040-Zero,23,RP2040
@@ -16,18 +16,18 @@ Architeuthis dux (a.dux),2,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
 articulation70,2,Elite-C,29,"ATmega32U4, RP2040"
 articulation80,2,Pro Micro,24,"ATmega32U4, RP2040"
 Atreis,1,Pro Micro,24,ATmega32U4
-atreus52,1,Onboard/SMD, ,RP2040
+atreus52,1,Onboard/SMD,0,RP2040
 atreus62,1,Pro Micro,24,"ATmega32U4, RP2040"
-Atreus62_FT,1,!Unknown, ,Unknown
+Atreus62_FT,1,!Unknown,0,Unknown
 Atreyu,1,Pro Micro,24,"ATmega32U4, RP2040"
 Atreyu v2,1,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
 Avalanche 40%,2,Pro Micro,24,ATmega32U4
 Avalanche 60%,2,Pro Micro,24,ATmega32U4
 AWKB,2,XIAO,14,RP2040
-Bad Wings,1,"XIAO, Onboard/SMD",14,RP2040
-Bancouver40,1,Onboard/SMD, ,!ATmega32U4-M
+Bad Wings,1,"Onboard/SMD, XIAO",0,RP2040
+Bancouver40,1,Onboard/SMD,0,!ATmega32U4-M
 Barghoot,1,Elite-C,29,"ATmega32U4, RP2040"
-Barobord,1,"Pro Micro, Onboard/SMD",24,"ATmega32U4, RP2040, nRF52840, !ATmega32U4-M"
+Barobord,1,"Onboard/SMD, Pro Micro",0,"ATmega32U4, RP2040, nRF52840, !ATmega32U4-M"
 Basilisk,2,Pi Pico,44,RP2040
 BFO-9000 (4x#),2,Pro Micro,24,"ATmega32U4, RP2040"
 BFO-9000 (5x#),2,Pro Micro,24,"ATmega32U4, RP2040"
@@ -40,14 +40,14 @@ Briq,2,Nice!Nano,29,nRF52840
 Brk Out,1,XIAO,14,"RP2040, nRF52840"
 Btrfld,1,Pro Micro,24,"ATmega32U4, nRF52840"
 BumWings,1,RP2040-Zero,23,RP2040
-Buran,1,Onboard/SMD, ,RP2040
+Buran,1,Onboard/SMD,0,RP2040
 Buteo,1,RP2040-Zero,23,RP2040
 Buzzard,2,Pro Micro,24,ATmega32U4
-Cantaloupe,2,Onboard/SMD, ,!STM32F401CC
-catboard,1,Onboard/SMD, ,!AVR-USB162MU
+Cantaloupe,2,Onboard/SMD,0,!STM32F401CC
+catboard,1,Onboard/SMD,0,!AVR-USB162MU
 CB34S,1,Pro Micro,24,nRF52840
 Centromere Plus,2...3,"Pro Micro, !Core51822 (B)",24,"ATmega32U4, !nRF51822"
-Ch55p34,1,Onboard/SMD, ,!CH552
+Ch55p34,1,Onboard/SMD,0,!CH552
 chakra56,2,Pro Micro,24,ATmega32U4
 Charybdis,2,Elite-C,29,RP2040
 Charybdis Mini,2,Elite-C,29,RP2040
@@ -59,16 +59,16 @@ Chocofly,1,Pro Micro,24,"ATmega32U4, nRF52840"
 Choctopus44,1,Pro Micro,24,"ATmega32U4, nRF52840"
 ChocV,1,Pro Micro,24,"ATmega32U4, nRF52840"
 ChonkV,1,Pro Micro,24,nRF52840
-ChonkyKong,1,Onboard/SMD, ,RP2040
+ChonkyKong,1,Onboard/SMD,0,RP2040
 Chortyl,2,Pro Micro,24,ATmega32U4
 Chrumm 1.0,1,Pi Pico,44,RP2040
 Chunky,2,Pi Pico,44,RP2040
 Churn,1,Nice!Nano,29,nRF52840
 Churn Plus,1,Nice!Nano,29,nRF52840
-city42,1,Onboard/SMD, ,RP2040
+city42,1,Onboard/SMD,0,RP2040
 Closis,1,Pro Micro,24,ATmega32U4
 Cockpit,1,Pro Micro,24,"ATmega32U4, nRF52840"
-cocot36plus,1,Onboard/SMD, ,RP2040
+cocot36plus,1,Onboard/SMD,0,RP2040
 cocot46plus,1,Pro Micro,24,ATmega32U4
 compression (4c),2,Pro Micro,24,nRF52840
 Corax56,2,Pro Micro,24,nRF52840
@@ -80,30 +80,30 @@ CozyKeys Bloomer,1,Elite-C,29,ATmega32U4
 CozyKeys Speedo,1,Elite-C,29,ATmega32U4
 crabapplepad,1,XIAO,14,nRF52840
 Crowboard,1,Pi Pico,44,RP2040
-Cut Slope,1,Onboard/SMD, ,RP2040
+Cut Slope,1,Onboard/SMD,0,RP2040
 cybr36,2,Pro Micro,24,nRF52840
 Cygnus 1.0,1,"RP2040-Zero, Pro Micro, Elite-C",23,"RP2040, ATmega32U4"
 Dactyl Generator,2,Pro Micro,24,ATmega32U4
 Dactyl Manuform,2,Pro Micro,24,ATmega32U4
-Dao Choc BLE,2,!MS88SF2,28,!MS88SF2
+Dao Choc BLE,2,!MS88SF2,28,nRF52840
 Darknight,2,!Teensy 3.2,33,!Teensy
 DELPHI,2,!QT Py,18,RP2040
 Demeter,2,Pro Micro,24,"ATmega32U4, nRF52840"
 DigDug,1,XIAO,14,"RP2040, nRF52840"
 Dilemma,2,"Pro Micro, Onboard/SMD",24,RP2040
 Dilemma Max,2,"Pro Micro, Onboard/SMD",24,RP2040
-Djinn,2,Onboard/SMD, ,!STM32G474RET6
+Djinn,2,Onboard/SMD,0,!STM32G474RET6
 DracuLad,2,Pro Micro,24,"ATmega32U4, nRF52840"
-DragonFruit,1,Onboard/SMD, ,RP2040
+DragonFruit,1,Onboard/SMD,0,RP2040
 Drift,2,RP2040-Zero,23,RP2040
 Duet,1,Nice!Nano,29,nRF52840
-Dygma Defy,1,Onboard/SMD, ,Unknown
+Dygma Defy,1,Onboard/SMD,0,Unknown
 Egboard,1,Pi Pico,44,RP2040
 Egg58,2,Pro Micro,24,ATmega32U4
 Egg58bt,2,Pro Micro,24,nRF52840
 elephant42,2,Pro Micro,24,ATmega32U4
-Elora,2,Onboard/SMD, ,RP2040
-Endeavour,1,Onboard/SMD, ,RP2040
+Elora,2,Onboard/SMD,0,RP2040
+Endeavour,1,Onboard/SMD,0,RP2040
 Equals 48,1,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
 Equals 60,1,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
 Ergo S-1,2,Nice!Nano,29,nRF52840
@@ -115,14 +115,14 @@ ErgoDash,2,Pro Micro,24,ATmega32U4
 ErgoDash_FT,2,Pro Micro,24,ATmega32U4
 ErgoDash LP,2,Pro Micro,24,ATmega32U4
 Ergodonk,1..2,Pro Micro,24,ATmega32U4
-ErgoDox EZ,2,Onboard/SMD, ,ATmega32U4
+ErgoDox EZ,2,Onboard/SMD,0,ATmega32U4
 Ergodox-like,1..3,"!Teensy 2.0, Onboard/SMD",31,"!Teensy 8-bit, !MK20DX256VLH7"
-ErgoDox_FT_LP,2,!Unknown, ,Unknown
+ErgoDox_FT_LP,2,!Unknown,0,Unknown
 Ergoinu,2,Pro Micro,24,ATmega32U4
-Ergolite,3,Onboard/SMD, ,Unknown
+Ergolite,3,Onboard/SMD,0,Unknown
 ErgoMax,2,Pro Micro,24,ATmega32U4
 Ergonaut One,2,XIAO,14,nRF52840
-ErgoNICE,1,Onboard/SMD, ,!STM32L151C8
+ErgoNICE,1,Onboard/SMD,0,!STM32L151C8
 Ergotonic 49,1,Pro Micro,24,ATmega32U4
 ErgoTravel,2,Pro Micro,24,ATmega32U4
 ErgoTravelXT,2,Pro Micro,24,ATmega32U4
@@ -130,15 +130,15 @@ Eskarp,2,Pi Pico,44,RP2040
 Essence,1,Elite-C,29,ATmega32U4
 ETERNAL Keypad,1,Pro Micro,24,ATmega32U4
 Explorer Mountain,1,Pro Micro,24,"ATmega32U4, !ATmega328"
-ExtraDox,2,Onboard/SMD, ,!ATmega32U4-M
+ExtraDox,2,Onboard/SMD,0,!ATmega32U4-M
 faux fox keyboard (ffkb) v3,1,Pro Micro,24,"RP2040, nRF52840"
-faux fox keyboard (ffkb) v4,1,Onboard/SMD, ,RP2040
+faux fox keyboard (ffkb) v4,1,Onboard/SMD,0,RP2040
 FAW36,2,XIAO,14,nRF52840
-Ferris 0.1,1,Onboard/SMD, ,ATmega32U4
-Ferris 0.1 - Compact,1,Onboard/SMD, ,ATmega32U4
-Ferris 0.2,1,Onboard/SMD, ,!STM32F072CBT6
-Ferris 0.2 - Bling,1,Onboard/SMD, ,!STM32F072CBT6
-Ferris 0.2 - High,1,Onboard/SMD, ,!STM32F072CBT6
+Ferris 0.1,1,Onboard/SMD,0,ATmega32U4
+Ferris 0.1 - Compact,1,Onboard/SMD,0,ATmega32U4
+Ferris 0.2,1,Onboard/SMD,0,!STM32F072CBT6
+Ferris 0.2 - Bling,1,Onboard/SMD,0,!STM32F072CBT6
+Ferris 0.2 - High,1,Onboard/SMD,0,!STM32F072CBT6
 Fifi,2,Pro Micro,24,ATmega32U4
 Fisk,2,RP2040-Zero,23,RP2040
 Fissure,2,Pro Micro,24,"ATmega32U4, nRF52840"
@@ -149,14 +149,14 @@ Fortitude60,2,!Beetle CM-32u4,16,ATmega32U4
 Frame 48,2,Pro Micro,24,RP2040
 Fulcrum (20),1,!ItsyBitsy,28,nRF52840
 Fulcrum (40),1,!ItsyBitsy,28,nRF52840
-Fusion,1,Onboard/SMD, ,nRF52840
+Fusion,1,Onboard/SMD,0,nRF52840
 gameboard,1,RP2040-Zero,23,RP2040
 Ghoul,1,!MicroMod,40,"!Teensy 32-bit, RP2040, !SAMD21"
 Gleb's Sexy Acrylic,1,RP2040-Zero,23,RP2040
-Glove80,2,Onboard/SMD, ,nRF52840
+Glove80,2,Onboard/SMD,0,nRF52840
 grumpy,1,XIAO,14,"RP2040, nRF52840, !SAMD21"
 Gull,1,Pro Micro,24,ATmega32U4
-Harper,2,Onboard/SMD, ,nRF52840
+Harper,2,Onboard/SMD,0,nRF52840
 Helix,2,Pro Micro,24,ATmega32U4
 Heron v1,1,Pro Micro,24,ATmega32U4
 Hillside 46,2,Pro Micro,24,"ATmega32U4, nRF52840"
@@ -164,17 +164,17 @@ Hillside 48,2,Pro Micro,24,"ATmega32U4, nRF52840"
 Hillside 52,2,Pro Micro,24,"ATmega32U4, nRF52840"
 Hillside 56,2,Pro Micro,24,"ATmega32U4, nRF52840"
 HillSideView,2,Pro Micro,24,"ATmega32U4, nRF52840"
-Honeydew,1,Onboard/SMD, ,!STM32F072RBT6
+Honeydew,1,Onboard/SMD,0,!STM32F072RBT6
 Horizon,1,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
 Hotreus62,1,Pro Micro,24,ATmega32U4
 Humla,1,Pro Micro,24,nRF52840
 hummingbird,1,XIAO,14,!SAMD21
-IF-CORNE,3,Onboard/SMD, ,Unknown
-IIICC,4,Onboard/SMD, ,"ATmega32U4, !MCP23018-E/MJ"
+IF-CORNE,3,Onboard/SMD,0,Unknown
+IIICC,4,Onboard/SMD,0,"ATmega32U4, !MCP23018-E/MJ"
 IK,2,RP2040-Zero,23,RP2040
 interphase,2,!Core51822 (B),36,!nRF51822
-Iris,2,Onboard/SMD, ,"RP2040, ATmega32U4"
-Iris CE,2,Onboard/SMD, ,RP2040
+Iris,2,Onboard/SMD,0,"RP2040, ATmega32U4"
+Iris CE,2,Onboard/SMD,0,RP2040
 Jaye 44,2,RP2040-Zero,23,RP2040
 JESK56,1,Elite-C,29,RP2040
 JiaEX,2,!Teensy++ 2.0,54,!Teensy 8-bit
@@ -189,21 +189,21 @@ Keyball39,2,Pro Micro,24,ATmega32U4
 Keyball44,2,Pro Micro,24,ATmega32U4
 Keyball46,2,Pro Micro,24,ATmega32U4
 Keyball61,2,Pro Micro,24,ATmega32U4
-Keyboardio Atreus,1,Onboard/SMD, ,ATmega32U4
-Keyboardio Model 100,2,Onboard/SMD, ,ATmega32U4
-KeyMouse Alpha,2,Onboard/SMD, ,Unknown
-KeyMouse Track,2,Onboard/SMD, ,Unknown
+Keyboardio Atreus,1,Onboard/SMD,0,ATmega32U4
+Keyboardio Model 100,2,Onboard/SMD,0,ATmega32U4
+KeyMouse Alpha,2,Onboard/SMD,0,Unknown
+KeyMouse Track,2,Onboard/SMD,0,Unknown
 Killer Whale,1,Pi Pico,44,RP2040
 Kimiko,2,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
-Kinesis Advantage 2,1,"Onboard/SMD, !Teensy++ 2.0", ,"Unknown, !Teensy 8-bit"
-Kinesis Advantage360,2,Onboard/SMD, ,nRF52840
+Kinesis Advantage 2,1,"Onboard/SMD, !Teensy++ 2.0",0,"Unknown, !Teensy 8-bit"
+Kinesis Advantage360,2,Onboard/SMD,0,!nRF52840-QIAA-R
 Klavgen,2,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
 Klavyl,2,Pro Micro,24,ATmega32U4
 KLOR,2,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
 KLOTZ,2,Pro Micro,24,nRF52840
 Kyria,2,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
-Le Capybara,1,Onboard/SMD, ,!STM32F072CBT6
-Le Chiffre,1,Onboard/SMD, ,!ATmega32U4-AU
+Le Capybara,1,Onboard/SMD,0,!STM32F072CBT6
+Le Chiffre,1,Onboard/SMD,0,!ATmega32U4-AU
 Lil Chonky Bois,2,"!BlueMicro840, !BlueMacro840",24,nRF52840
 Lily58,2,Pro Micro,24,ATmega32U4
 Lotus 58,2,Pro Micro,24,ATmega32U4
@@ -212,31 +212,31 @@ Lumberjack,1,!IC socket,28,!ATMEGA328P-PU
 Luna / ルナ,2,Pro Micro,24,nRF52840
 Lunakey,2,"Pro Micro, Pi Pico",24,"ATmega32U4, RP2040"
 Lynx,2,!Banana Pi BPI-Leaf,44,!ESP32-S3
-Lyra,2,Onboard/SMD, ,!ATmega32U4-MU
+Lyra,2,Onboard/SMD,0,!ATmega32U4-MU
 Lörtsy,1,!Black Pill,44,"!STM32F401, !STM32F411"
-Lütt un Lütt,2,Onboard/SMD, ,nRF52840
-Maltron L90,1,Onboard/SMD, ,Unknown
+Lütt un Lütt,2,Onboard/SMD,0,nRF52840
+Maltron L90,1,Onboard/SMD,0,Unknown
 Manta60,2,Pro Micro,24,"ATmega32U4, nRF52840"
 Mantis,1,Pro Micro,24,ATmega32U4
 Mask,2,Pro Micro,24,ATmega32U4
 Microdox,2,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
-minipeg48,1,Onboard/SMD, ,ATmega32U4
+minipeg48,1,Onboard/SMD,0,ATmega32U4
 MM60Ergo,1,Pro Micro,24,ATmega32U4
 Morizon,1,Pro Micro,24,"ATmega32U4, nRF52840"
-Mün 2,2,Onboard/SMD, ,RP2040
-NeoDox,2,Onboard/SMD, ,RP2040
+Mün 2,2,Onboard/SMD,0,RP2040
+NeoDox,2,Onboard/SMD,0,RP2040
 Nijuni,2,Pro Micro,24,"ATmega32U4, nRF52840"
-NISSE,1,Onboard/SMD, ,!PIC18F4550
+NISSE,1,Onboard/SMD,0,!PIC18F4550
 Niztyl,2,"Elite-C, !Teensy 2.0",29,"ATmega32U4, RP2040, !Teensy 8-bit"
-Nyquist,2,Onboard/SMD, ,ATmega32U4
+Nyquist,2,Onboard/SMD,0,ATmega32U4
 Nyx,2,Pro Micro,24,nRF52840
 oddball,2,Elite-C,29,ATmega32U4
 OK35,2,Pro Micro,24,ATmega32U4
 Ortholily,2,Pro Micro,24,ATmega32U4
-Osprey,1,Onboard/SMD, ,ATmega32U4
+Osprey,1,Onboard/SMD,0,ATmega32U4
 Pangaea,2,Pro Micro,24,ATmega32U4
 Penguin,2,Pro Micro,24,ATmega32U4
-Phobos,2,Onboard/SMD, ,Unknown
+Phobos,2,Onboard/SMD,0,Unknown
 Pi5,1,Pi Pico,44,RP2040
 Piantor,2,Pi Pico,44,RP2040
 pica40,2,XIAO,14,nRF52840
@@ -254,7 +254,7 @@ Pragmatic 56,1,Pro Micro,24,ATmega32U4
 Pragmatic 60,1,Pro Micro,24,ATmega32U4
 Prime52,2,RP2040-Zero,23,RP2040
 Proteus67,1,Elite-C,29,"ATmega32U4, RP2040"
-Protocol Seven,1,Onboard/SMD, ,ATmega32U4
+Protocol Seven,1,Onboard/SMD,0,ATmega32U4
 Pteron 3x5,1..2,"Pro Micro, !BlueMicro840",24,"ATmega32U4, nRF52840"
 Pteron 3x6,1,Pro Micro,24,ATmega32U4
 Pteron 4x6,1,Pro Micro,24,ATmega32U4
@@ -266,84 +266,84 @@ Rebound-S,1,"Pro Micro, !Montsinger Mainframe",24,ATmega32U4
 Redox,2,Pro Micro,24,"ATmega32U4, nRF52840"
 Redox FT,2..3,"Pro Micro, !Core51822 (B)",24,"ATmega32U4, nRF52840"
 Redox FT LP,2..3,"Pro Micro, !Core51822 (B)",24,"ATmega32U4, nRF52840"
-Redox Manuform,2,Pro Micro,24,#N/A
-Redpoll,1,Pro Micro,24,#N/A
-ReRedox,2,Pi Pico,44,#N/A
-Rev57LP,1,Pro Micro,24,#N/A
-Reviung41,1,Pro Micro,24,#N/A
-Reviung41 with trackball,1,Pro Micro,24,#N/A
-revxlp,1,XIAO,14,#N/A
-Rhymestone,2,Pro Micro,24,#N/A
-Rolio,2,Pro Micro,24,#N/A
-Rommana,2,XIAO,14,#N/A
-Santoku,1,Elite-C,29,#N/A
-SectorC,2,Pro Micro,24,#N/A
-SectorD,2,Pro Micro,24,#N/A
-SectorH,2,Pro Micro,24,#N/A
-Seismos,2,Pro Micro,24,#N/A
-Sentinel,1,"Onboard/SMD, !OmegaSupreme", ,#N/A
-shortstack,2,Pro Micro,24,#N/A
-Signum 3.0,1,"Pro Micro, !Teensy 2.0, Elite-C",24,#N/A
-Signum 3.1,1,"Pro Micro, !Teensy 2.0, Elite-C",24,#N/A
-simplyKeeb 60K,2,Pro Micro,24,#N/A
-Skywatch,1,Onboard/SMD, ,RP2040
-slabV,1,Pro Micro,24,#N/A
-Smol,2,RP2040-Zero,23,#N/A
-Smudge,2,XIAO,14,!QT Py RP2040
-Sofle,2,Pro Micro,24,#N/A
-Sofle Choc,2,Pro Micro,24,#N/A
-Sofle Choc Wireless,2,Pro Micro,24,#N/A
-Sofle FT,2,Pro Micro,24,#N/A
-Sofle FT LP,2,Pro Micro,24,#N/A
-Sofle Hybrid,2,Pro Micro,24,#N/A
-Sofle PLUS (xcmkb),2,Pro Micro,24,#N/A
-sok42,2,Onboard/SMD, ,!CH32V203C8T6
-Sol 3,2,Onboard/SMD, ,#N/A
-Sotka,2,Pro Micro,24,#N/A
-Soufflé,2,Pro Micro,24,RP2040
-Span,2,Pro Micro,24,#N/A
-splaytoraid,1,Elite-C,29,#N/A
+Redox Manuform,2,Pro Micro,24,ATmega32U4
+Redpoll,1,Nice!Nano,29,nRF52840
+ReRedox,2,Pi Pico,44,RP2040
+Rev57LP,1,Pro Micro,24,"RP2040, ATmega32U4"
+Reviung41,1,Pro Micro,24,ATmega32U4
+Reviung41 with trackball,1,Pro Micro,24,ATmega32U4
+revxlp,1,XIAO,14,"!SAMD21, nRF52840, RP2040"
+Rhymestone,2,Pro Micro,24,ATmega32U4
+Rolio,2,Pro Micro,24,nRF52840
+Rommana,2,XIAO,14,nRF52840
+Santoku,1,Elite-C,29,ATmega32U4
+SectorC,2,Pro Micro,24,ATmega32U4
+SectorD,2,Pro Micro,24,ATmega32U4
+SectorH,2,Pro Micro,24,ATmega32U4
+Seismos,2,Pro Micro,24,nRF52840
+Sentinel,1,"Onboard/SMD, !OmegaSupreme",0,RP2040
+shortstack,2,Pro Micro,24,"ATmega32U4, RP2040"
+Signum 3.0,1,"Pro Micro, !Teensy 2.0, Elite-C",24,"ATmega32U4, !Teensy 8-bit"
+Signum 3.1,1,"Pro Micro, !Teensy 2.0, Elite-C",24,"ATmega32U4, !Teensy 8-bit"
+simplyKeeb 60K,1..2,"Onboard/SMD, Pro Micro",0,"ATmega32U4, RP2040"
+Skywatch,1,Onboard/SMD,0,RP2040
+slabV,1,Pro Micro,24,ATmega32U4
+Smol,2,RP2040-Zero,23,RP2040
+Smudge,2,XIAO,14,RP2040
+Sofle,2,Pro Micro,24,ATmega32U4
+Sofle Choc,2,Pro Micro,24,ATmega32U4
+Sofle Choc Wireless,2,Pro Micro,24,nRF52840
+Sofle FT,2,Pro Micro,24,"ATmega32U4, nRF52840"
+Sofle FT LP,2,Pro Micro,24,"ATmega32U4, nRF52840"
+Sofle Hybrid,2,Pro Micro,24,"RP2040, nRF52840"
+Sofle PLUS (xcmkb),2,Pro Micro,24,RP2040
+sok42,3,Onboard/SMD,0,"!CH32V203C8T6, !CH32V208CBU6"
+Sol 3,2,Onboard/SMD,0,!STM32
+Sotka,2,Pro Micro,24,ATmega32U4
+Soufflé,2,Pro Micro,24,"RP2040, ATmega32U4, nRF52840"
+Span,2,Pro Micro,24,RP2040
+splaytoraid,1,Pro Micro,24,"RP2040, nRF52840"
 Spleeb,2,Pro Micro,24,RP2040
 split_fire,1,Pi Pico,44,RP2040
-splitreus62,2,Pro Micro,24,#N/A
-StarShip-Centurion,1,Onboard/SMD, ,!VIK
-Storyboard,1,!MicroMod,40,#N/A
-stront,2,RP2040-Zero,23,#N/A
+splitreus62,2,Pro Micro,24,ATmega32U4
+StarShip-Centurion,1,Onboard/SMD,0,RP2040
+Storyboard,1,!MicroMod,40,RP2040
+stront,2,RP2040-Zero,23,RP2040
 Sugar Glider,1,!Black Pill,44,"!STM32F401, !STM32F411"
-supersplit,2,Pro Micro,24,#N/A
-Sweep,2,Pro Micro,24,#N/A
-swepp,2,Elite-C,29,#N/A
-Taira,2,Pro Micro,24,#N/A
-tamago60,1,Pro Micro,24,#N/A
+supersplit,2,Pro Micro,24,ATmega32U4
+Sweep,2,Pro Micro,24,"ATmega32U4, nRF52840"
+swepp,2,Elite-C,29,RP2040
+Taira,2,Pro Micro,24,nRF52840
+tamago60,1,Pro Micro,24,"ATmega32U4, nRF52840"
 tamatama,2,RP2040-Zero,23,RP2040
-TE Cleave,1,Onboard/SMD, ,#N/A
-TeeShirt,2,Pro Micro,24,#N/A
-Tern,1,XIAO,14,#N/A
-Thorium,3,XIAO,14,#N/A
-Thumbs Up!,1..2,Pro Micro,24,#N/A
+TE Cleave,1,Onboard/SMD,0,Unknown
+TeeShirt,2,Pro Micro,24,"ATmega32U4, nRF52840"
+Tern,1,XIAO,14,RP2040
+Thorium,3,XIAO,14,nRF52840
+Thumbs Up!,1..2,Pro Micro,24,"ATmega32U4, RP2040, nRF52840"
 Torn,2,!IC socket,28,!Atmega328P-PU
-TOTEM,2,XIAO,14,#N/A
-Triboard,2,XIAO,14,#N/A
-TSBYM,2,Pro Micro,24,#N/A
-Unicorne,1,Onboard/SMD, ,!STM32F411
-uninarf,1,XIAO,14,#N/A
-unnamed 36 split,2,XIAO,14,#N/A
-Untitled keyboard,2,Pro Micro,24,#N/A
-Urchin,2,Pro Micro,24,#N/A
-Ursa Minor,1,Onboard/SMD, ,#N/A
-ut22,1,Pro Micro,24,#N/A
+TOTEM,2,XIAO,14,"RP2040, nRF52840"
+Triboard,2,XIAO,14,"nRF52840, RP2040, !ESP32C3"
+TSBYM,2,Pro Micro,24,ATmega32U4
+Unicorne,1,Onboard/SMD,0,!STM32F411
+uninarf,1,XIAO,14,nRF52840
+unnamed 36 split,2,XIAO,14,nRF52840
+Untitled keyboard,2,Pro Micro,24,nRF52840
+Urchin,2,Pro Micro,24,nRF52840
+Ursa Minor,1..2,"Onboard/SMD, Pro Micro",0,"ATmega32U4, !ATmega32U4-AU"
+ut22,1,Nice!Nano,29,nRF52840
 Vesuveus,1,"RP2040-Zero, Pro Micro",23,RP2040
-Viterbi,2,Pro Micro,24,#N/A
-Wave,2,Elite-C,29,#N/A
+Viterbi,2,Pro Micro,24,ATmega32U4
+Wave,2,Elite-C,29,ATmega32U4
 Willow64,2,Pro Micro,24,#N/A
-X-2,1,!Blue Pill,44,!STM32F103C8T6
-ximi,2,Onboard/SMD, ,RP2040
-YAEMK,2,Onboard/SMD, ,"!STM32F303CCT6, !GD32VF103CBT6"
-Zaphod Lite,1,XIAO,14,#N/A
+X-2,1,"!Blue Pill, !Black Pill",44,"!STM32F103C8T6, !STM32F401, !STM32F411"
+ximi,2,Onboard/SMD,0,RP2040
+YAEMK,2,Onboard/SMD,0,"!STM32F303CCT6, !GD32VF103CBT6"
+Zaphod Lite,1,XIAO,14,"RP2040, nRF52840"
 Zerosprey42,1,RP2040-Zero,23,RP2040
-Zilpzalp,1,XIAO,14,#N/A
-Zireael,2,!MS88SF2,28,!MS88SF2
+Zilpzalp,1,XIAO,14,"RP2040, nRF52840, !CH552T"
+Zireael,2,!MS88SF2,28,nRF52840
 Zodiark,2,"RP2040-Zero, Pro Micro, !Black Pill",23,RP2040
-ZSA Moonlander,2,Onboard/SMD, ,!STM32F303
-ZSA Voyager,2,Onboard/SMD, ,#N/A
+ZSA Moonlander,2,Onboard/SMD,0,!STM32F303
+ZSA Voyager,2,Onboard/SMD,0,!STM32F303xC
 `
